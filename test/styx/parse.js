76a87b59ee3b9c7aa42fb2e1ad80ecaee4dc1135
Marius Schulz <marius.schulz@me.com>
2015-04-29 17:19:01 +0200

    Tests that empty program returns a single flow node

--- test/styx/parse.js
+++ test/styx/parse.js
@@ -1,15 +1,28 @@
 var expect = chai.expect;
 
 describe("Styx", function() {
+    function controlFlowGraphFor(code) {
+        var ast = esprima.parse(code);
+        var cfg = Styx.parse(ast);
+
+        return cfg;
+    }
+
     describe("#parse()", function() {
         it("should return a control flow graph", function() {
-            var ast = { type: "Program", body: [] };
-
-            var cfg = Styx.parse(ast);
+            var cfg = controlFlowGraphFor("");
 
             expect(cfg).to.be.an("object");
             expect(cfg).to.have.property("entry");
             expect(cfg.entry).to.be.an("object");
+        });
+    });
+
+    describe("#parseProgram()", function() {
+        it("should return a control flow graph with a single node", function() {
+            var cfg = controlFlowGraphFor("");
+
+            expect(cfg.entry.next).to.be.empty;
         });
     });
 });
