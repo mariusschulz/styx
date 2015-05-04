704039af6129094d268b27617ab2bbb7d71e318b
Marius Schulz <marius.schulz@me.com>
2015-05-04 14:36:47 +0200

    Adds more ESTree types

--- src/estree.ts
+++ src/estree.ts
@@ -4,6 +4,9 @@ module ESTree {
         static Program = "Program";
         static VariableDeclaration = "VariableDeclaration";
     }
+    
+    
+    // Node objects
 
     export interface Node {
         type: string;
@@ -20,12 +23,59 @@ module ESTree {
         line: number; // >= 1
         column: number; // >= 0
     }
+    
+    
+    // Programs
 
     export interface Program extends Node {
         body: Statement[];
     }
 
+
+    // Statements
+
     export interface Statement extends Node {
 
+    }
+    
+    
+    // Declarations
+    
+    export interface Declaration extends Statement {
+        
+    }
+    
+    export interface VariableDeclaration extends Declaration {
+        declarations: VariableDeclarator[];
+    }
+    
+    export interface VariableDeclarator extends Node {
+        id: Pattern;
+        init?: Expression;
+    }
+    
+    
+    // Expressions 
+    
+    interface Expression extends Node {
+        
+    }
+    
+    
+    // Patterns
+    
+    interface Pattern extends Node {
+        
+    }
+    
+    
+    // Miscellaneous
+    
+    interface Identifier extends Node, Expression, Pattern {
+        name: string;
+    }
+    
+    interface Literal extends Node, Expression {
+        value?: string | boolean | number | RegExp
     }
 }
