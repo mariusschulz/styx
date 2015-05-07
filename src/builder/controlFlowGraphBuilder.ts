0bd69f357184f5e0ace9e1360692291ee0163c92
Marius Schulz <marius.schulz@me.com>
2015-05-07 13:42:49 +0200

    Introduces 'ConstructionContext' for node creation + adds node id

--- src/builder/controlFlowGraphBuilder.ts
+++ src/builder/controlFlowGraphBuilder.ts
@@ -1,33 +1,48 @@
 /// <reference path="../estree.ts"/>
+/// <reference path="../types.ts"/>
 
 module Styx.ControlFlowGraphBuilder {
+    interface ConstructionContext {
+        createNode: () => FlowNode;
+    }
+    
     export function constructGraphFor(program: ESTree.Program): ControlFlowGraph {
-        return parseProgram(program);
+        let makeNewId = (function() {
+            let id = 0;
+            return () => ++id;
+        }());
+        
+        let constructionContext = {
+            createNode: () => new FlowNode(makeNewId())
+        };
+        
+        return parseProgram(program, constructionContext);
     }
 
-    function parseProgram(program: ESTree.Program): ControlFlowGraph {
-        var cfg = new ControlFlowGraph();
+    function parseProgram(program: ESTree.Program, context: ConstructionContext): ControlFlowGraph {
+        let entryNode = context.createNode();
+        let flowGraph = new ControlFlowGraph(entryNode);
 
-        parseStatements(program.body, cfg.entry);
+        parseStatements(program.body, flowGraph.entry, context);
 
-        return cfg;
+        return flowGraph;
     }
 
-    function parseStatements(statements: ESTree.Statement[], currentFlowNode: FlowNode) {
+    function parseStatements(statements: ESTree.Statement[], currentFlowNode: FlowNode, context: ConstructionContext) {
         for (let statement of statements) {
-            currentFlowNode = parseStatement(statement, currentFlowNode);
+            currentFlowNode = parseStatement(statement, currentFlowNode, context);
         }
     }
 
-    function parseStatement(statement: ESTree.Statement, currentFlowNode: FlowNode): FlowNode {
+    function parseStatement(statement: ESTree.Statement, currentFlowNode: FlowNode, context: ConstructionContext): FlowNode {
         if (statement.type === ESTree.NodeType.EmptyStatement) {
-            currentFlowNode = createNodeAndAppendTo(currentFlowNode);
+            currentFlowNode = context.createNode().appendTo(currentFlowNode);
         } else if (statement.type === ESTree.NodeType.VariableDeclaration) {
             let declaration = <ESTree.VariableDeclaration>statement;
-            currentFlowNode = parseVariableDeclaration(declaration, currentFlowNode);
+            currentFlowNode = parseVariableDeclaration(declaration, currentFlowNode, context);
         } else if (statement.type === ESTree.NodeType.IfStatement) {
             let ifStatement = <ESTree.IfStatement>statement;
-            currentFlowNode = parseIfStatement(ifStatement, currentFlowNode);
+            currentFlowNode = parseIfStatement(ifStatement, currentFlowNode, context);
         } else {
             throw Error(`Encountered unsupported statement type '${statement.type}'`);
         }
@@ -35,23 +50,23 @@ module Styx.ControlFlowGraphBuilder {
         return currentFlowNode;
     }
 
-    function parseVariableDeclaration(declaration: ESTree.VariableDeclaration, currentFlowNode: FlowNode): FlowNode {
+    function parseVariableDeclaration(declaration: ESTree.VariableDeclaration, currentFlowNode: FlowNode, context: ConstructionContext): FlowNode {
         for (let declarator of declaration.declarations) {
-            currentFlowNode = createNodeAndAppendTo(currentFlowNode);
+            currentFlowNode = context.createNode().appendTo(currentFlowNode);
         }
 
         return currentFlowNode;
     }
 
-    function parseIfStatement(ifStatement: ESTree.IfStatement, currentFlowNode: FlowNode): FlowNode {
+    function parseIfStatement(ifStatement: ESTree.IfStatement, currentFlowNode: FlowNode, context: ConstructionContext): FlowNode {
         return ifStatement.alternate === null
-            ? parseSimpleIfStatement(ifStatement, currentFlowNode)
-            : parseIfElseStatement(ifStatement, currentFlowNode);
+            ? parseSimpleIfStatement(ifStatement, currentFlowNode, context)
+            : parseIfElseStatement(ifStatement, currentFlowNode, context);
     }
 
-    function parseSimpleIfStatement(ifStatement: ESTree.IfStatement, currentFlowNode: FlowNode): FlowNode {
-        let ifNode = createNodeAndAppendTo(currentFlowNode);
-        let finalNode = createNodeAndAppendTo(ifNode);
+    function parseSimpleIfStatement(ifStatement: ESTree.IfStatement, currentFlowNode: FlowNode, context: ConstructionContext): FlowNode {
+        let ifNode = context.createNode().appendTo(currentFlowNode);
+        let finalNode = context.createNode().appendTo(ifNode);
 
         let edgeToFinalNode = new FlowEdge(finalNode);
         currentFlowNode.addOutgoingEdge(edgeToFinalNode);
@@ -59,22 +74,10 @@ module Styx.ControlFlowGraphBuilder {
         return finalNode;
     }
 
-    function parseIfElseStatement(ifStatement: ESTree.IfStatement, currentFlowNode: FlowNode): FlowNode {
-        let ifNode = createNodeAndAppendTo(currentFlowNode);
-        let elseNode = createNodeAndAppendTo(currentFlowNode);
-
-        return createNodeAndAppendTo(ifNode, elseNode);
-    }
-
-    function createNodeAndAppendTo(node: FlowNode, ...otherNodes: FlowNode[]): FlowNode {
-        let newNode = new FlowNode();
-        let nodes = [node, ...otherNodes];
-
-        for (let node of nodes) {
-            let edge = new FlowEdge(newNode);
-            node.addOutgoingEdge(edge);
-        }
-
-        return newNode;
+    function parseIfElseStatement(ifStatement: ESTree.IfStatement, currentFlowNode: FlowNode, context: ConstructionContext): FlowNode {
+        let ifNode = context.createNode().appendTo(currentFlowNode);
+        let elseNode = context.createNode().appendTo(currentFlowNode);
+        
+        return context.createNode().appendTo(ifNode, elseNode);
     }
 }
--- src/types.ts
+++ src/types.ts
@@ -2,20 +2,33 @@ module Styx {
     export class ControlFlowGraph {
         entry: FlowNode;
 
-        constructor() {
-            this.entry = new FlowNode();
+        constructor(entry: FlowNode) {
+            this.entry = entry;
         }
     }
 
     export class FlowNode {
+        id: number;
         outgoingEdges: FlowEdge[];
 
-        constructor() {
+        constructor(id: number) {
+            this.id = id;
             this.outgoingEdges = [];
         }
 
         addOutgoingEdge(edge: FlowEdge) {
             this.outgoingEdges.push(edge);
+        }
+
+        appendTo(node: FlowNode, ...otherNodes: FlowNode[]): FlowNode {
+            let nodes = [node, ...otherNodes];
+    
+            for (let node of nodes) {
+                let edge = new FlowEdge(this);
+                node.addOutgoingEdge(edge);
+            }
+    
+            return this;
         }
     }
 
