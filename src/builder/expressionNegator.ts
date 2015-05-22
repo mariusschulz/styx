cf8a776a300dfc9c62c761c7897928b5aeb21f94
Marius Schulz <marius.schulz@me.com>
2015-05-22 13:14:45 +0200

    Wraps non-boolean-literals in unary negation expressions

--- src/builder/expressionNegator.ts
+++ src/builder/expressionNegator.ts
@@ -1,4 +1,5 @@
 /// <reference path="../estree.ts" />
+
 module Styx.ExpressionNegator {
     export function safelyNegate(expression: ESTree.Expression): ESTree.Expression {
         if (expression.type === ESTree.NodeType.Literal) {
@@ -9,13 +10,22 @@ module Styx.ExpressionNegator {
             }
         }
         
-        throw Error("Not implemented");
+        return wrapInUnaryNegationExpression(expression);
     }
     
     function createBooleanLiteral(value: boolean): ESTree.Literal {
         return {
             type: ESTree.NodeType.Literal,
             value: value
+        };
+    }
+    
+    function wrapInUnaryNegationExpression(expression: ESTree.Expression): ESTree.UnaryExpression {
+        return {
+            type: ESTree.NodeType.UnaryExpression,
+            operator: "!",
+            prefix: true,
+            argument: expression
         };
     }
 }
