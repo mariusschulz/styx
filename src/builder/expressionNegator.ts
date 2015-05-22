cc43ff3c59c1e63e783513f124bf333e07657a64
Marius Schulz <marius.schulz@me.com>
2015-05-22 13:27:24 +0200

    Inverts equality comparison operators

--- src/builder/expressionNegator.ts
+++ src/builder/expressionNegator.ts
@@ -8,6 +8,12 @@ module Styx.ExpressionNegator {
             if (typeof literal.value === "boolean") {
                 return createBooleanLiteral(!literal.value);
             }
+        } else if (expression.type === ESTree.NodeType.BinaryExpression) {
+            let binaryExpression = <ESTree.BinaryExpression>expression;
+            
+            if (["==", "===", "!=", "!=="].indexOf(binaryExpression.operator) > -1) {
+                return invertEqualityComparisonOperator(binaryExpression);
+            }
         }
         
         return wrapInUnaryNegationExpression(expression);
@@ -17,6 +23,19 @@ module Styx.ExpressionNegator {
         return {
             type: ESTree.NodeType.Literal,
             value: value
+        };
+    }
+    
+    function invertEqualityComparisonOperator(binaryExpression: ESTree.BinaryExpression): ESTree.BinaryExpression {
+        let isNegated = binaryExpression.operator.charAt(0) === "!";
+        let firstCharOfInvertedOperator = isNegated ? "=" : "!";
+        let restOfInvertedOperator = binaryExpression.operator.substr(1);
+        
+        return {
+            type: ESTree.NodeType.BinaryExpression,
+            operator: firstCharOfInvertedOperator + restOfInvertedOperator,
+            left: binaryExpression.left,
+            right: binaryExpression.right            
         };
     }
     
