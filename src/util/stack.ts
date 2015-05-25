0c86444fa45eebdf18223e265f484990f8685ab6
Marius Schulz <marius.schulz@me.com>
2015-05-25 16:09:44 +0200

    Introduces Stack<T> helper class

---
+++ src/util/stack.ts
@@ -0,0 +1,17 @@
+module Styx.Util {
+    export class Stack<T> {
+        private elements: T[];
+        
+        constructor() {
+            this.elements = [];
+        }
+        
+        push(element: T): void {
+            this.elements.push(element);
+        }
+        
+        pop(): T {
+            return this.elements[this.elements.length - 1];
+        }
+    }
+}
