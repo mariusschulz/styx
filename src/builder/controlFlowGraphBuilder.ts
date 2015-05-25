720be614bc888a8e2a87c5ad0263f713777020a1
Marius Schulz <marius.schulz@me.com>
2015-05-25 11:52:48 +0200

    Inlines type casts to avoid unnecessary copying to local variable

--- src/builder/controlFlowGraphBuilder.ts
+++ src/builder/controlFlowGraphBuilder.ts
@@ -183,28 +183,23 @@ module Styx {
         
         parseExpression(expression: ESTree.Expression, currentNode: FlowNode): FlowNode {
             if (expression.type === ESTree.NodeType.AssignmentExpression) {
-                let assignmentExpression = <ESTree.AssignmentExpression>expression;
-                return this.parseAssignmentExpression(assignmentExpression, currentNode);
+                return this.parseAssignmentExpression(<ESTree.AssignmentExpression>expression, currentNode);
             }
             
             if (expression.type === ESTree.NodeType.UpdateExpression) {
-                let updateExpression = <ESTree.UpdateExpression>expression;
-                return this.parseUpdateExpression(updateExpression, currentNode);
+                return this.parseUpdateExpression(<ESTree.UpdateExpression>expression, currentNode);
             }
             
             if (expression.type === ESTree.NodeType.SequenceExpression) {
-                let sequenceExpression = <ESTree.SequenceExpression>expression;
-                return this.parseSequenceExpression(sequenceExpression, currentNode);
+                return this.parseSequenceExpression(<ESTree.SequenceExpression>expression, currentNode);
             }
             
             if (expression.type === ESTree.NodeType.CallExpression) {
-                let callExpression = <ESTree.CallExpression>expression;
-                return this.parseCallExpression(callExpression, currentNode);
+                return this.parseCallExpression(<ESTree.CallExpression>expression, currentNode);
             }
             
             if (expression.type === ESTree.NodeType.NewExpression) {
-                let newExpression = <ESTree.NewExpression>expression;
-                return this.parseNewExpression(newExpression, currentNode);
+                return this.parseNewExpression(<ESTree.NewExpression>expression, currentNode);
             }
             
             throw Error(`Encountered unsupported expression type '${expression.type}'`);
