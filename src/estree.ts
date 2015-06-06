f5079750ec0addf33dfefba6d7ab3089c324a671
Marius Schulz <marius.schulz@me.com>
2015-06-06 12:04:53 +0200

    Deletes unused source location types

--- src/estree.ts
+++ src/estree.ts
@@ -36,20 +36,8 @@ module ESTree {
 
     export interface Node {
         type: string;
-        loc?: SourceLocation;
     }
 
-    export interface SourceLocation {
-        source?: string;
-        start: Position;
-        end: Position;
-    }
-
-    export interface Position {
-        line: number; // >= 1
-        column: number; // >= 0
-    }
-    
     
     // Programs
 
