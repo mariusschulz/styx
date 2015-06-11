9e7a9c9211846966999d795f4dcd8d40fa1c5534
Marius Schulz <marius.schulz@me.com>
2015-06-11 11:44:49 +0200

    Implements 'Set<T>' collection

---
+++ src/collections/set.ts
@@ -0,0 +1,20 @@
+namespace Styx.Collections {
+    type ObjectKey = string | number;
+    type LookupObject = { [key: string]: boolean; } 
+    
+    export class Set<T extends ObjectKey> {
+        private elementLookup: LookupObject;
+        
+        constructor() {
+            this.elementLookup = {};
+        }
+        
+        add(value: T) {
+            this.elementLookup[value.toString()] = true;
+        }
+        
+        contains(value: T) {
+            return this.elementLookup.hasOwnProperty(value.toString());
+        }
+    }
+}
