58eb204941c3111bcdd45de50cf420cfb6bf3266
Marius Schulz <marius.schulz@me.com>
2015-06-11 11:58:10 +0200

    Simplifies more cases

--- src/parser/passes/edgeOptimization.ts
+++ src/parser/passes/edgeOptimization.ts
@@ -21,19 +21,30 @@ namespace Styx.Passes {
             let outgoingEdge = node.outgoingEdges[0];
             let target = outgoingEdge.target;
             
-            if (outgoingEdge.type === EdgeType.Epsilon && target.incomingEdges.length === 1) {
-                let incomingEdge = node.incomingEdges[0];
+            if (outgoingEdge.type === EdgeType.Epsilon) {
+                let sourceAlreadyConnectedToTarget = false;
                 
-                // Redirect edge
-                incomingEdge.target = target;
-                target.incomingEdges = [incomingEdge];
+                for (let incomingTargetEdges of target.incomingEdges) {
+                     if (incomingTargetEdges.source.id === node.incomingEdges[0].source.id) {
+                         sourceAlreadyConnectedToTarget = true;
+                         break;
+                     }
+                }
                 
-                // Clear node
-                node.incomingEdges = [];
-                node.outgoingEdges = [];
+                if (!sourceAlreadyConnectedToTarget) {                
+                    let incomingEdge = node.incomingEdges[0];
+                    
+                    // Redirect edge
+                    incomingEdge.target = target;
+                    target.incomingEdges = [incomingEdge];
+                    
+                    // Clear node
+                    node.incomingEdges = [];
+                    node.outgoingEdges = [];
                 
-                // Recursively optimize
-                optimizeNode(target, optimizedNodes);
+                    // Recursively optimize
+                    optimizeNode(target, optimizedNodes);
+                }
             }
         }
         
