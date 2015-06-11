a020d74951157246aa92983475f339bff936d0fe
Marius Schulz <marius.schulz@me.com>
2015-06-11 10:25:12 +0200

    Simple epsilon edge optimization

--- src/parser/passes/edgeOptimization.ts
+++ src/parser/passes/edgeOptimization.ts
@@ -1,7 +1,26 @@
 /// <reference path="../../flow.ts" />
 
 namespace Styx.Passes {
-    export function optimizeEdges(cfg: ControlFlowGraph) {
-        console.log("Optimizing edges ...");
+    export function optimizeEdges(graph: ControlFlowGraph) {
+        optimizeNode(graph.entry);
+    }
+    
+    function optimizeNode(node: FlowNode) {
+        console.log(`Optimizing node ${node.id} ...`);
+        
+        if (node.incomingEdges.length === 1 && node.outgoingEdges.length === 1) {
+            let [incomingEdge] = node.incomingEdges;
+            let [outgoingEdge] = node.outgoingEdges;
+            
+            if (outgoingEdge.type === EdgeType.Epsilon) {
+                incomingEdge.target = outgoingEdge.target;
+                node.incomingEdges = [];
+                node.outgoingEdges = [];
+            }
+        }
+        
+        for (let edge of node.outgoingEdges) {
+            optimizeNode(edge.target);
+        }
     }
 }
