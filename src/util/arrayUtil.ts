a3bd2771c4f2b2dd4e5edf4ccb7a1885003e89d0
Marius Schulz <marius.schulz@me.com>
2015-06-17 15:58:13 +0200

    Adds function to remove an element from an array

---
+++ src/util/arrayUtil.ts
@@ -0,0 +1,9 @@
+module Styx.Util.Arrays {
+    export function removeElementFromArray<T>(element: T, array: T[]) {
+        for (let i = array.length - 1; i >= 0; i--) {
+            if (array[i] === element) {
+                array.splice(i, 1);
+            }
+        }
+    }
+}
