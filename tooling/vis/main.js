c8510e73860eefc7c00e8cef02efa2d421565c75
Marius Schulz <marius.schulz@me.com>
2015-06-18 11:39:46 +0200

    Puts jQuery extension into separate file

--- tooling/vis/main.js
+++ tooling/vis/main.js
@@ -16,21 +16,6 @@
     
     initializeFormFromSessionStorage();
     
-    $.fn.setCaretPosition = function(position) {
-        return this.each(function() {
-            if (this.setSelectionRange) {
-                this.focus();
-                this.setSelectionRange(position, position);
-            } else if (this.createTextRange) {
-                var range = this.createTextRange();
-                range.collapse(true);
-                range.moveEnd("character", position);
-                range.moveStart("character", position);
-                range.select();
-            }
-        });
-    };
-    
     function update() {
         var code = $input.val();
         var options = {
