09e3c3e83a0b30672308b73e3c3bd72332c3e2a1
Marius Schulz <marius.schulz@me.com>
2015-07-01 13:41:20 +0200

    Rewrites function expressions

---
+++ src/parser/ast-preprocessing/functionExpressionRewriter.ts
@@ -0,0 +1,54 @@
+/// <reference path="../../util/idGenerator.ts" />
+/// <reference path="../../estree.ts" />
+
+namespace Styx.Parser.AstPreprocessing {
+    interface FunctionExpressionToRewrite {
+        name: string;
+        functionExpression: ESTree.FunctionExpression;
+    }
+    
+    export function rewriteFunctionExpressions(program: ESTree.Program): ESTree.Program {
+        let functionIdGenerator = Util.IdGenerator.create();
+        let functionExpressionsToRewrite: FunctionExpressionToRewrite[] = [];
+        
+        let stringifiedProgram = JSON.stringify(program, visitNode);
+        let clonedProgram: ESTree.Program = JSON.parse(stringifiedProgram);
+        
+        for (let func of functionExpressionsToRewrite) {
+            let functionDeclaration: ESTree.Function = {
+                type: ESTree.NodeType.FunctionDeclaration,
+                id: { type: ESTree.NodeType.Identifier, name: func.name },
+                params: func.functionExpression.params,
+                body: func.functionExpression.body
+            };
+            
+            clonedProgram.body.unshift(functionDeclaration);
+        }
+        
+        return clonedProgram;
+    
+        function visitNode(key: string, value: any): any {
+            return value && value.type === ESTree.NodeType.FunctionExpression
+                ? rewriteFunctionExpression(value)
+                : value;
+        }
+        
+        function rewriteFunctionExpression(functionExpression: ESTree.FunctionExpression): ESTree.Identifier {
+            let funcName = "$$func" + functionIdGenerator.generateId();
+            
+            if (functionExpression.id) {
+                funcName += "_" + functionExpression.id.name;
+            }
+            
+            functionExpressionsToRewrite.push({
+                name: funcName,
+                functionExpression: functionExpression
+            });
+            
+            return {
+                type: ESTree.NodeType.Identifier,
+                name: funcName
+            }
+        }
+    }
+}
