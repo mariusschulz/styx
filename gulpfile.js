be540695c39356db0ae02471444f70cd73117213
Marius Schulz <marius.schulz@me.com>
2015-07-10 00:55:52 +0200

    Renames "browser.js" to "styx.js" in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require("gulp");
 var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
 
 var transpiledScriptGlobs = ["./dist/transpiled/**/*.js"];
 
 gulp.task("browserify", function() {
-    return gulp.src("./dist/transpiled/index.js")
+    return gulp.src("./dist/transpiled/browser.js")
         .pipe(browserify())
+        .pipe(rename("styx.js"))
         .pipe(gulp.dest("./dist/browser"));
 });
 
