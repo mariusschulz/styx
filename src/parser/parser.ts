9d331b4a090a0dcd8da5c7be0b8333f87392c837
Marius Schulz <marius.schulz@me.com>
2015-07-11 14:22:20 +0200

    Moves parsing functions for function declarations to separate module

--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -27,8 +27,9 @@ import {
 
 import { parseIfStatement } from "./statements/if";
 import { parseSwitchStatement } from "./statements/switch";
+import { parseFunctionDeclaration } from "./statements/function";
 
-export { parse, parseStatement, parseStatements };
+export { parse, parseBlockStatement, parseStatement, parseStatements };
 
 interface StatementTypeToParserMap {
     [type: string]: (statement: ESTree.Statement, currentNode: FlowNode, context: ParsingContext) => Completion;
@@ -150,58 +151,6 @@ function parseStatement(statement: ESTree.Statement, currentNode: FlowNode, cont
     }
 
     return parsingMethod(statement, currentNode, context);
-}
-
-function parseFunctionDeclaration(functionDeclaration: ESTree.Function, currentNode: FlowNode, context: ParsingContext): Completion {
-    let entryNode = context.createNode(NodeType.Entry);
-    let successExitNode = context.createNode(NodeType.SuccessExit);
-    let errorExitNode = context.createNode(NodeType.ErrorExit);
-
-    let func: FlowFunction = {
-        id: context.createFunctionId(),
-        name: functionDeclaration.id.name,
-        flowGraph: {
-            entry: entryNode,
-            successExit: successExitNode,
-            errorExit: errorExitNode,
-            nodes: [],
-            edges: []
-        }
-    };
-
-    let functionContext: ParsingContext = {
-        functions: context.functions,
-        currentFlowGraph: func.flowGraph,
-
-        enclosingStatements: Stack.create<EnclosingStatement>(),
-
-        createTemporaryLocalVariableName: context.createTemporaryLocalVariableName,
-        createNode: context.createNode,
-        createFunctionId: context.createFunctionId
-    };
-
-    let completion = parseBlockStatement(functionDeclaration.body, entryNode, functionContext);
-
-    if (completion.normal) {
-        // If we reached this point, the function didn't end with an explicit return statement.
-        // Thus, an implicit "undefined" is returned.
-        let undefinedReturnValue: ESTree.Identifier = {
-            type: ESTree.NodeType.Identifier,
-            name: "undefined"
-        };
-
-        let returnStatement: ESTree.ReturnStatement = {
-            type: ESTree.NodeType.ReturnStatement,
-            argument: undefinedReturnValue
-        };
-
-        func.flowGraph.successExit
-            .appendTo(completion.normal, "return undefined", EdgeType.AbruptCompletion, returnStatement);
-    }
-
-    context.functions.push(func);
-
-    return { normal: currentNode };
 }
 
 function parseEmptyStatement(emptyStatement: ESTree.EmptyStatement, currentNode: FlowNode, context: ParsingContext): Completion {
---
+++ src/parser/statements/function.ts
@@ -0,0 +1,71 @@
+import { negateTruthiness } from "../expressions/negator";
+import { stringify } from "../expressions/stringifier";
+
+import { parseBlockStatement } from "../parser";
+
+import { Stack } from "../../collections/stack";
+
+import * as ESTree from "../../estree";
+import {
+    Completion,
+    EdgeType,
+    EnclosingStatement,
+    FlowFunction,
+    FlowNode,
+    NodeType,
+    ParsingContext
+} from "../../flow";
+
+export { parseFunctionDeclaration };
+
+function parseFunctionDeclaration(functionDeclaration: ESTree.Function, currentNode: FlowNode, context: ParsingContext): Completion {
+    let entryNode = context.createNode(NodeType.Entry);
+    let successExitNode = context.createNode(NodeType.SuccessExit);
+    let errorExitNode = context.createNode(NodeType.ErrorExit);
+
+    let func: FlowFunction = {
+        id: context.createFunctionId(),
+        name: functionDeclaration.id.name,
+        flowGraph: {
+            entry: entryNode,
+            successExit: successExitNode,
+            errorExit: errorExitNode,
+            nodes: [],
+            edges: []
+        }
+    };
+
+    let functionContext: ParsingContext = {
+        functions: context.functions,
+        currentFlowGraph: func.flowGraph,
+
+        enclosingStatements: Stack.create<EnclosingStatement>(),
+
+        createTemporaryLocalVariableName: context.createTemporaryLocalVariableName,
+        createNode: context.createNode,
+        createFunctionId: context.createFunctionId
+    };
+
+    let completion = parseBlockStatement(functionDeclaration.body, entryNode, functionContext);
+
+    if (completion.normal) {
+        // If we reached this point, the function didn't end with an explicit return statement.
+        // Thus, an implicit "undefined" is returned.
+        let undefinedReturnValue: ESTree.Identifier = {
+            type: ESTree.NodeType.Identifier,
+            name: "undefined"
+        };
+
+        let returnStatement: ESTree.ReturnStatement = {
+            type: ESTree.NodeType.ReturnStatement,
+            argument: undefinedReturnValue
+        };
+
+        func.flowGraph.successExit
+            .appendTo(completion.normal, "return undefined", EdgeType.AbruptCompletion, returnStatement);
+    }
+
+    context.functions.push(func);
+
+    return { normal: currentNode };
+}
