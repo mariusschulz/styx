1c68447df1d2a5a71f01efa48488ad7108681e27
Marius Schulz <marius.schulz@me.com>
2015-07-22 00:24:47 +0200

    Exports control flow graphs of program and all its functions

--- src/exporters/json.ts
+++ src/exporters/json.ts
@@ -1,18 +1,53 @@
-import { FlowProgram } from "../flow";
-
 export { exportJson };
 
-function exportJson(program: FlowProgram): string {
-    let nodes = program.flowGraph.nodes.map(node => ({
+import {
+    ControlFlowGraph,
+    EdgeType,
+    FlowProgram,
+    NodeType,
+} from "../flow";
+
+interface FlatFlowNode {
+    id: number;
+    type: NodeType;
+}
+
+interface FlatFlowEdge {
+    from: number;
+    to: number;
+    type: EdgeType;
+}
+
+interface FlatControlFlowGraph {
+    nodes: FlatFlowNode[];
+    edges: FlatFlowEdge[];
+}
+
+function exportJson(flowProgram: FlowProgram): string {
+    const program = {
+        flowGraph: flattenFlowGraph(flowProgram.flowGraph)
+    };
+
+    const functions = flowProgram.functions.map(fun => ({
+        id: fun.id,
+        name: fun.name,
+        flowGraph: flattenFlowGraph(fun.flowGraph)
+    }));
+
+    return JSON.stringify({ program, functions }, null, 2);
+}
+
+function flattenFlowGraph(flowGraph: ControlFlowGraph): FlatControlFlowGraph {
+    const nodes = flowGraph.nodes.map(node => ({
         id: node.id,
         type: node.type
     }));
 
-    let edges = program.flowGraph.edges.map(edge => ({
+    const edges = flowGraph.edges.map(edge => ({
         from: edge.source.id,
         to: edge.target.id,
         type: edge.type
     }));
 
-    return JSON.stringify({ nodes, edges }, null, 2);
+    return { nodes, edges };
 }
