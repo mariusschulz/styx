656e1df796595166ac1af06d90fb32778188f981
Marius Schulz <marius.schulz@me.com>
2015-08-02 16:51:24 +0200

    Adds an option to produce minified JSON

--- src/exporters/json.ts
+++ src/exporters/json.ts
@@ -6,8 +6,10 @@ import { FlowProgram } from "../flow";
 const REPLACER: any = null;
 const INDENTATION_STRING = "  ";
 
-function exportAsJson(flowProgram: FlowProgram) {
+function exportAsJson(flowProgram: FlowProgram, { pretty = true }: { pretty?: boolean } = { pretty: true }): string {
     let exportedObject = exportAsObject(flowProgram);
 
-    return JSON.stringify(exportedObject, REPLACER, INDENTATION_STRING);
+    return pretty
+        ? JSON.stringify(exportedObject, REPLACER, INDENTATION_STRING)
+        : JSON.stringify(exportedObject);
 }
