42dd2eb5ccf5cf89bcba56a4d9e40caedf4ab584
Marius Schulz <marius.schulz@me.com>
2015-08-08 17:14:12 +0200

    Breaks up parsing of different try/catch/finally productions into separate functions

--- src/parser/statements/try.ts
+++ src/parser/statements/try.ts
@@ -11,11 +11,16 @@ import {
 
 export { parseTryStatement };
 
+interface ParsedFinalizer {
+    bodyEntry: FlowNode;
+    bodyCompletion: Completion;
+}
+
 function parseTryStatement(tryStatement: ESTree.TryStatement, currentNode: FlowNode, context: ParsingContext): Completion {
     let handler = tryStatement.handlers[0];
     let finalizer = tryStatement.finalizer;
 
-    let parseFinalizer = () => {
+    let parseFinalizer: () => ParsedFinalizer = () => {
         let finalizerBodyEntry = context.createNode();
         let finalizerBodyCompletion = parseBlockStatement(finalizer, finalizerBodyEntry, context);
 
@@ -50,41 +55,53 @@ function parseTryStatement(tryStatement: ESTree.TryStatement, currentNode: FlowN
 
     context.enclosingStatements.pop();
 
-    // try/catch production
+    // try/catch production?
     if (handler && !finalizer) {
+        return parseTryCatch(tryBlockCompletion, handlerBodyCompletion, context);
+    }
+
+    // try/finally production?
+    if (!handler && finalizer) {
+        return parseTryFinally(tryBlockCompletion, parseFinalizer, context);
+    }
+
+    // try/catch/finally production
+    return parseTryCatchFinally(tryBlockCompletion, handlerBodyCompletion, parseFinalizer, context);
+}
+
+function parseTryCatch(tryBlockCompletion: Completion, handlerBodyCompletion: Completion, context: ParsingContext): Completion {
+    let finalNode = context.createNode();
+
+    if (tryBlockCompletion.normal) {
+        finalNode.appendEpsilonEdgeTo(tryBlockCompletion.normal);
+    }
+
+    if (handlerBodyCompletion.normal) {
+        finalNode.appendEpsilonEdgeTo(handlerBodyCompletion.normal);
+    }
+
+    return { normal: finalNode };
+}
+
+function parseTryFinally(tryBlockCompletion: Completion, parseFinalizer: () => ParsedFinalizer, context: ParsingContext): Completion {
+    if (!tryBlockCompletion.normal) {
+        return tryBlockCompletion;
+    }
+
+    let finalizer = parseFinalizer();
+    finalizer.bodyEntry.appendEpsilonEdgeTo(tryBlockCompletion.normal);
+
+    if (finalizer.bodyCompletion.normal) {
         let finalNode = context.createNode();
-
-        if (tryBlockCompletion.normal) {
-            finalNode.appendEpsilonEdgeTo(tryBlockCompletion.normal);
-        }
-
-        if (handlerBodyCompletion.normal) {
-            finalNode.appendEpsilonEdgeTo(handlerBodyCompletion.normal);
-        }
+        finalNode.appendEpsilonEdgeTo(finalizer.bodyCompletion.normal);
 
         return { normal: finalNode };
     }
 
-    // try/finally production
-    if (!handler && finalizer) {
-        if (!tryBlockCompletion.normal) {
-            return tryBlockCompletion;
-        }
+    return finalizer.bodyCompletion;
+}
 
-        let finalizer = parseFinalizer();
-        finalizer.bodyEntry.appendEpsilonEdgeTo(tryBlockCompletion.normal);
-
-        if (finalizer.bodyCompletion.normal) {
-            let finalNode = context.createNode();
-            finalNode.appendEpsilonEdgeTo(finalizer.bodyCompletion.normal);
-
-            return { normal: finalNode };
-        }
-
-        return finalizer.bodyCompletion;
-    }
-
-    // try/catch/finally production
+function parseTryCatchFinally(tryBlockCompletion: Completion, handlerBodyCompletion: Completion, parseFinalizer: () => ParsedFinalizer, context: ParsingContext): Completion {
     let finalNode = context.createNode();
 
     if (tryBlockCompletion.normal) {
